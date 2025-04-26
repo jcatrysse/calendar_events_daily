a7d61a503086e10db2c76da2ab87a37cec456137
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2025-04-26 14:09:15 +0200

    * GUI correction * Correct references to plugin name

--- app/hooks/calendar_legend_hook.rb
+++ app/hooks/calendar_legend_hook.rb
@@ -4,7 +4,7 @@ module CalendarLegendHook
 
     def view_layouts_base_html_head(context)
       javascript_tag("var calendar_events_daily_between = \"#{I18n.t('calendar_events_daily.between')}\";") +
-       javascript_include_tag(:calendar_events_daily, plugin: 'redmine_calendar_events_daily')
+       javascript_include_tag(:calendar_events_daily, plugin: 'calendar_events_daily')
     end
   end
 end
